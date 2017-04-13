3b77bad3b238c75e7f52d2c236c9b7d5647c5aeb
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-13 13:50:34 +0300

    Improve structure: reformat to new API

--- models/contract.py
+++ models/contract.py
@@ -2,40 +2,6 @@ from openerp.osv import osv, fields
 from openerp.tools.translate import _
 import openerp.addons.decimal_precision as dp
 
-class hr_contract_finnish(osv.osv):
-    _name="hr.contract"
-    _inherit="hr.contract"
-    
-    _columns = {
-        'tax_card_type': fields.selection((('A', 'A'), ('B', 'B')), _('Tax card type'), required=False),
-        'base_tax_percentage': fields.float('Base tax percentage (%)', required=False),
-        'extra_tax_percentage': fields.float('Extra tax percentage (%)', required=False),
-        'yearly_income_limit': fields.float('Yearly income limit', required=False),
-        'monthly_income_limit': fields.float('Monthly income limit', required=False),
-        
-        'benefit_car': fields.float('Car benefit', required=False),
-        'benefit_apartment': fields.float('Apartment benefit', required=False),
-        'benefit_lunch': fields.float('Lunch benefit', required=False),
-        'benefit_other': fields.float('Other benefit', required=False),
-        'membership_fee':fields.float('Membership fee (%)'),
-        'union':fields.char('Union'),
-    }
-    
-    _defaults = {
-        
-        'base_tax_percentage': 0.0,
-        'extra_tax_percentage': 0.0,
-        'yearly_income_limit': 0.0,
-        'monthly_income_limit': 0.0,
-        
-        'benefit_car': 0.0,
-        'benefit_apartment': 0.0,
-        'benefit_lunch': 0.0,
-        'benefit_other': 0.0,
-    }
-
-hr_contract_finnish()
-
 class hr_payslip_input(osv.osv):
     _inherit='hr.payslip.input'
     _columns={
---
+++ models/hr_contract.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from openerp import api, fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class HrContract(models.Model):
+    
+    # 1. Private attributes
+    _inherit = 'hr.contract'
+
+    # 2. Fields declaration
+    tax_card_type = fields.Selection((('A', 'A'), ('B', 'B')), 'Tax card type', required=False)
+    base_tax_percentage = fields.Float('Base tax percentage (%)', required=False, default=0.0)
+    extra_tax_percentage = fields.Float('Extra tax percentage (%)', required=False, default=0.0)
+    yearly_income_limit = fields.Float('Yearly income limit', required=False, default=0.0)
+    monthly_income_limit = fields.Float('Monthly income limit', required=False, default=0.0)
+
+    benefit_car = fields.Float('Car benefit', required=False, default=0.0)
+    benefit_apartment = fields.Float('Apartment benefit', required=False, default=0.0)
+    benefit_lunch = fields.Float('Lunch benefit', required=False, default=0.0)
+    benefit_other = fields.Float('Other benefit', required=False, default=0.0)
+    membership_fee = fields.Float('Membership fee (%)')
+    union = fields.Char('Union'),
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
