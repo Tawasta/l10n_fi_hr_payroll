67d04e7147639f0b49a8ce1b6b403abad696dd45
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-19 15:12:15 +0300

    Add salary rule "Puhelinkorvaus"

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Finnish Salary',
     'summary': 'Adds fields to HR contracts for calculating Finnish salaries',
-    'version': '8.0.1.2.17',
+    'version': '8.0.1.2.18',
     'category': 'Human Resources',
     'website': 'http://www.plan1st.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
