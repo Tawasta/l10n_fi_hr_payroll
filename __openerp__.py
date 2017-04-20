cab9116fe422cc3ea35fc3e1b65477f7e6fd8bfc
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-20 12:24:41 +0300

    Add salary rule "Yöpymiskorvaus"

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Finnish Salary',
     'summary': 'Adds fields to HR contracts for calculating Finnish salaries',
-    'version': '8.0.1.3.15',
+    'version': '8.0.1.3.16',
     'category': 'Human Resources',
     'website': 'http://www.plan1st.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
