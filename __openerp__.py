84d874f1cf33e41e88eb025e578909e710ab40ed
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-20 12:02:56 +0300

    Add salary rule "Työnantajan TyEL-maksu"

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Finnish Salary',
     'summary': 'Adds fields to HR contracts for calculating Finnish salaries',
-    'version': '8.0.1.3.2',
+    'version': '8.0.1.3.3',
     'category': 'Human Resources',
     'website': 'http://www.plan1st.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
